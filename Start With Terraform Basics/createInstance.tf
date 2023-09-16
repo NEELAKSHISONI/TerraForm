c63810fcb34305ad732b32f6b26dd821bb13eff8
NEELAKSHISONI <113483146+NEELAKSHISONI@users.noreply.github.com>
2023-09-16 14:53:09 -0700

    change

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,9 +1,4 @@
-provider "aws" {
-    version = "3.53.0"
-    access_key = "AKIAQAABTBCAHBDKYVY6"
-    secret_key = "SECRET_KEY"
-    region     = "us-east-2"
-}
+
 resource "aws_instance" "MyFirstInstnace" {
   ami           = "ami-05692172625678b4e"
   instance_type = "t2.micro"
